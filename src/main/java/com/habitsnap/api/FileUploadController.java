778f19d1f6a8d88a0ec3e98dc425bbc4926b2ea0
Ye-jee <yejee0408@gmail.com>
2025-11-11 22:18:45 +0900

    EXIF 메타데이터 추출 로직 구현(Week3 Day2)

--- src/main/java/com/habitsnap/api/FileUploadController.java
+++ src/main/java/com/habitsnap/api/FileUploadController.java
@@ -1,6 +1,8 @@
 package com.habitsnap.api;
 
 import com.habitsnap.application.FileStorageService;
+import com.habitsnap.application.LocalFileStorageService;
+import com.habitsnap.dto.MealUploadResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,12 +18,15 @@ import org.springframework.http.MediaType;
 @RequiredArgsConstructor
 public class FileUploadController {
 
-    private final FileStorageService fileStorageService;
+    private final LocalFileStorageService fileStorageService;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.saveFile(file);
-        return ResponseEntity.ok("파일이 성공적으로 업로드됨: " + fileName);
+    public ResponseEntity<MealUploadResponse> uploadFile(@RequestParam("file") MultipartFile file) {
+        MealUploadResponse response = fileStorageService.saveFile(file);
+        return ResponseEntity.ok(response);
+
+        /*String fileName = fileStorageService.saveFile(file);
+        return ResponseEntity.ok("파일이 성공적으로 업로드됨: " + fileName);*/
     }
 
 
